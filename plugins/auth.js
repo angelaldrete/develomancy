cadf638db64c791848405c906664d14ad8e65e60
Angel <angel.aldrete96@gmail.com>
2021-04-06 19:42:49 -0700

    Added env variables

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,7 +2,7 @@ export default function ({ $axios }, inject) {
 
   const auth = $axios.create()
 
-  auth.setBaseURL('http://localhost:4000/api')
+  auth.setBaseURL(`${process.env.AUTH_API_URL}/api` || 'http://localhost:4000/api')
 
   inject('auth', auth)
 
