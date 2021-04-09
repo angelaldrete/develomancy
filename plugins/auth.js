a57a954ab9e7c4e64ac16653e03b366ff7f4f00c
Angel <angel.aldrete96@gmail.com>
2021-04-08 18:51:09 -0700

    updated sign up urls

--- plugins/auth.js
+++ plugins/auth.js
@@ -2,7 +2,7 @@ export default function ({ $axios }, inject) {
 
   const auth = $axios.create()
 
-  auth.setBaseURL('https://develomancy-auth-api.herokuapp.com/api')
+  auth.setBaseURL(process.env.NODE_ENV !== 'production' ? 'http://localhost:4000/api' : 'https://develomancy-auth-api.herokuapp.com/api')
 
   inject('auth', auth)
 
